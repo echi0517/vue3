96b6a33f42e26ad99aaf481d1df36925c666d2aa
echi0517 <echi0517@naver.com>
2024-05-14 11:50:01 +0900

    class style vue 학습

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
-	<div></div>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus odit
+			corporis maxime optio recusandae, architecto culpa ullam nisi iure impedit
+			nulla blanditiis eveniet quia, repudiandae neque nam fugit deserunt quas!
+		</div>
+		<button>-</button>
+		<button>+</button>
+	</div>
 </template>
 
 <script>
+import { computed, reactive, ref } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+		const fontSize = ref(13);
+		const styleObject = computed(() => {
+			return {
+				color: 'blue',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return {
+			styleObject,
+		};
 	},
 };
 </script>
---
+++ src/lectures/9/App_class_binding.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<div class="text" :class="classObject">텍스트입니다</div>
+		<button @click="toggle()">toggle</button>
+		<button @click="hasError = !hasError">toggle</button>
+	</div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+	setup() {
+		const isActive = ref(true);
+		const hasError = ref(false);
+		// const classObject = ref({
+		// 	active: isActive,
+		// 	'text-danger': hasError,
+		// });
+
+		const classObject = computed(() => {
+			return {
+				active: isActive.value,
+				'text-danger': hasError.value,
+			};
+		});
+		const toggle = () => (isActive.value = !isActive.value);
+
+		return {
+			isActive,
+			toggle,
+			hasError,
+			classObject,
+		};
+	},
+};
+</script>
+
+<style scoped>
+.active {
+	font-weight: 900;
+}
+.text-danger {
+	color: red;
+}
+</style>
