46f47bdcc213f5bc8c3b988bbd3122f8615ef28a
echi0517 <echi0517@naver.com>
2024-05-22 09:25:14 +0900

    디렉티브 + 이벤트처리 방법

---
+++ src/lectures/12/App_directives.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<p>{{ msg }}</p>
+		<p v-text="msg"></p>
+		<p v-html="htmlStr"></p>
+		<p>{{ msg }}</p>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const msg = ref('안녕하세요');
+		const htmlStr = ref('<b> 안녕!! </b>');
+		return {
+			msg,
+			htmlStr,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/lectures/12/App_v-memo.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<div v-memo="[views]">
+			<p>subscrubers : {{ subscrubers }}</p>
+			<p>views : {{ views }}</p>
+			<p>likes : {{ likes }}</p>
+		</div>
+		<button @click="subscrubers++">Sub++</button>
+		<button @click="views++">Views++</button>
+		<button @click="likes++">likes++</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const subscrubers = ref(4000);
+		const views = ref(400);
+		const likes = ref(2);
+		return {
+			subscrubers,
+			views,
+			likes,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
